53a15a714a75d9a629eafac8e000370050f74415
lakolosy <christian.travelers@proton.me>
2025-09-26 03:55:02 +0200

    Add files via upload

---
+++ churches.js
@@ -0,0 +1,13 @@
+const churches = [
+  ["Ekar Andraisoro", -18.90419490035995, 47.555616745350854, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Soamanandrariny", -18.89035437710012, 47.56520277164898, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Mangasoavina", -18.903361771026482, 47.53741660237425, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Ambohimirary", -18.897597293526662, 47.551462080657224, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Antsobolo", -18.913151981722137, 47.568183792929936, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Ambohipo", -18.92563028530179, 47.556232811976315, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Mangasoavina","Antananarivo"],
+  ["Ekar Andohalo", -18.917760615223497, 47.53075866600047, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Andohalo","Antananarivo"],
+  ["Ekar Faravohitra", -18.904749836370616, 47.52802031271467, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Andohalo","Antananarivo"],
+  ["Ekar Antanimena", -18.89822785120746, 47.52263078643001, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Andohalo","Antananarivo"],
+  ["Ekar Antohomadinika", -18.897955418197643, 47.514951834586846, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Andohalo","Antananarivo"],
+  ["Chapelle Betongolo", -18.905160986116083, 47.53837580639343, "Sabotsy: 17:30 <br>Alahady: 07:00, 09:00, 18:00","Andohalo","Antananarivo"]
+];
